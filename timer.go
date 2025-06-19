f3143d23131bd6fe8c0e92947a732f47f9acab8b
possiblyHugo <alainhandzar@gmail.com>
2025-06-19 10:58:53 -0700

    Timer Module with error handling

--- timer.go
+++ timer.go
@@ -5,10 +5,33 @@ import (
 	"time"
 )
 
-func main() {
-	timer1 := time.NewTimer(time.Second)
+type SecondsTimer struct {
+	timer     *time.Timer
+	end       time.Time
+	remaining time.Duration
+}
 
-	fmt.Printf("%d\n", timer1)
-	fmt.Printf("%d\n", timer1)
+func NewSecondsTimer(t time.Duration) (*SecondsTimer, error) {
+	if t <= 0 {
+		return nil, fmt.Errorf("Invalid Duration: Must be greater than zero")
+	}
+	return &SecondsTimer{time.NewTimer(t), time.Now().Add(t), time.Second}, nil
+}
 
+func (s *SecondsTimer) Reset(t time.Duration) {
+	s.timer.Reset(t)
+	s.end = time.Now().Add(t)
+}
+
+func (s *SecondsTimer) Stop() {
+	s.timer.Stop()
+	s.remaining = s.TimeRemaining() // Save time for later use in the Resume Method
+}
+
+func (s *SecondsTimer) Resume() {
+	s.timer = time.NewTimer(s.remaining)
+}
+
+func (s *SecondsTimer) TimeRemaining() time.Duration {
+	return s.end.Sub(time.Now())
 }
